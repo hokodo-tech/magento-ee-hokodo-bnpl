09799390813448911725427af820271bb2a0dc30
MartyMcFly85 <>
2023-01-11 14:38:40 +0200

    rename hokodo tab hide eligibility status if no company assigned

--- view/adminhtml/web/js/component/form/company-credit-limit.js
+++ view/adminhtml/web/js/component/form/company-credit-limit.js
@@ -6,6 +6,10 @@ define([
     'Magento_Catalog/js/price-utils'
 ], function ($, ko, _, Component, priceUtils) {
     return Component.extend({
+        isCompanyIdAssigned() {
+            return !!this.source.data.hokodo.company_id;
+        },
+
         getFormattedAmount(amount) {
             return priceUtils.formatPrice(amount, {pattern:'%s ' + this.source.data.hokodo.credit_limit.currency}, false)
         }
