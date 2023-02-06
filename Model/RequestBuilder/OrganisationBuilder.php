c48e1fda72baef657889dccf9bdbe07c32c2b994
MartyMcFly85 <>
2023-02-06 16:41:00 +0200

    added preference to change hokodo organisation creation request using magento company id in it

---
+++ Model/RequestBuilder/OrganisationBuilder.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Hokodo\BnplCommerce\Model\RequestBuilder;
+
+use Hokodo\BNPL\Api\Data\Gateway\CreateOrganisationRequestInterface;
+use Hokodo\BNPL\Api\Data\Gateway\CreateOrganisationRequestInterfaceFactory;
+use Hokodo\BNPL\Api\HokodoEntityTypeResolverInterface;
+use Hokodo\BnplCommerce\Model\Config\Source\EntityLevelForSave;
+use Magento\Company\Api\CompanyManagementInterface;
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Store\Model\StoreManagerInterface;
+
+class OrganisationBuilder extends \Hokodo\BNPL\Model\RequestBuilder\OrganisationBuilder
+{
+    /**
+     * @var HokodoEntityTypeResolverInterface
+     */
+    private HokodoEntityTypeResolverInterface $hokodoEntityTypeResolver;
+
+    /**
+     * @var CreateOrganisationRequestInterfaceFactory
+     */
+    private CreateOrganisationRequestInterfaceFactory $createOrganisationGatewayRequestFactory;
+
+    /**
+     * @var CompanyManagementInterface
+     */
+    private CompanyManagementInterface $companyManagement;
+
+    /**
+     * @var CustomerRepositoryInterface
+     */
+    private CustomerRepositoryInterface $customerRepository;
+
+    /**
+     * OrganisationBuilder constructor.
+     *
+     * @param CreateOrganisationRequestInterfaceFactory $createOrganisationGatewayRequestFactory
+     * @param StoreManagerInterface                     $storeManager
+     * @param HokodoEntityTypeResolverInterface         $hokodoEntityTypeResolver
+     * @param CompanyManagementInterface                $companyManagement
+     * @param CustomerRepositoryInterface               $customerRepository
+     */
+    public function __construct(
+        CreateOrganisationRequestInterfaceFactory $createOrganisationGatewayRequestFactory,
+        StoreManagerInterface $storeManager,
+        HokodoEntityTypeResolverInterface $hokodoEntityTypeResolver,
+        CompanyManagementInterface $companyManagement,
+        CustomerRepositoryInterface $customerRepository
+    ) {
+        parent::__construct(
+            $createOrganisationGatewayRequestFactory,
+            $storeManager
+        );
+        $this->hokodoEntityTypeResolver = $hokodoEntityTypeResolver;
+        $this->createOrganisationGatewayRequestFactory = $createOrganisationGatewayRequestFactory;
+        $this->companyManagement = $companyManagement;
+        $this->customerRepository = $customerRepository;
+    }
+
+    /**
+     * Build hokodo organisation based on entity setup.
+     *
+     * @param string $companyId
+     * @param string $userEmail
+     *
+     * @return CreateOrganisationRequestInterface
+     *
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    public function build(string $companyId, string $userEmail = ''): CreateOrganisationRequestInterface
+    {
+        if ($this->hokodoEntityTypeResolver->resolve() === EntityLevelForSave::COMPANY) {
+            $gatewayRequest = $this->createOrganisationGatewayRequestFactory->create();
+            $company = $this->companyManagement->getByCustomerId($this->customerRepository->get($userEmail)->getId());
+            return $gatewayRequest
+                ->setCompanyId($companyId)
+                ->setUniqueId('mage-org-' . hash('md5', $companyId . $company->getId()))
+                ->setRegistered(date('Y-m-d\TH:i:s\Z'));
+        }
+
+        return parent::build($companyId, $userEmail);
+    }
+}
